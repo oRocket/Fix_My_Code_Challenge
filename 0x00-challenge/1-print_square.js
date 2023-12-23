c72b3fc091d22202acf5cbc3474153242c9b27c7
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2023-12-23 08:29:53 +0000

    Print square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -4,18 +4,18 @@
     of the program.
 */
 
+
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1);
+    process.exit(1)
 }
 
-// Corrected line: use parseInt without base
-let size = parseInt(process.argv[2]);
+size = parseInt(process.argv[2], 10)
 
-for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
+for (let i = 0 ; i < size ; i ++) {
+    for (let j = 0 ; j < size ; j ++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
